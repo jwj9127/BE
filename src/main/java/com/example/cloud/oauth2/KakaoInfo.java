c2389b75a0fe27330dc9e1ca9046d88295f56bd5
jwj9127 <jwj9127@naver.com>
2025-01-20 02:54:08 +0900

    CI/CD 적용, 배포 및 소셜 로그인 진행 중

---
+++ src/main/java/com/example/cloud/oauth2/KakaoInfo.java
@@ -0,0 +1,13 @@
+package com.example.cloud.oauth2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoInfo {
+    private Long id;
+    private String nickname;
+}
