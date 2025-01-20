f266ca01b4248e61186fbb6d17d9ee93949b9e59
jwj9127 <jwj9127@naver.com>
2025-01-20 20:53:35 +0900

    [feat]: 카카오 서비스 작성중

--- src/main/java/com/example/cloud/oauth2/OAuthService.java
+++ src/main/java/com/example/cloud/oauth2/OAuthService.java
@@ -1,12 +1,8 @@
 package com.example.cloud.oauth2;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -36,35 +32,6 @@ public class OAuthService {
         String accessToken = responseBody.substring(responseBody.indexOf("access_token\":\"") + 15);
         accessToken = accessToken.substring(0, accessToken.indexOf("\""));
         return accessToken;
-    }
-
-    public KakaoInfo getKakaoInfo(String accessToken) throws JsonProcessingException {
-        // HTTP Header 생성
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        // HTTP 요청 보내기
-        HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> response = rt.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoUserInfoRequest,
-                String.class
-        );
-
-        // responseBody에 있는 정보 꺼내기
-        String responseBody = response.getBody();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseBody);
-
-        Long id = jsonNode.get("id").asLong();
-        String email = jsonNode.get("kakao_account").get("email").asText();
-        String nickname = jsonNode.get("properties")
-                .get("nickname").asText();
-
-        return new KakaoInfo(id, nickname);
     }
 
     public String getUserInfo(String accessToken) {
